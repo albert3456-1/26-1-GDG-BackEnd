4bc66a9d908c36ff69ead1eaf2d7075d9ef2f80b
albert3456-1 <yund46237@gmail.com>
2026-04-03 22:04:57 +0900

    2주차 최종제출

--- shop/src/main/java/com/example/shop/member/MemberController.java
+++ shop/src/main/java/com/example/shop/member/MemberController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.lang.reflect.Member;
+//import java.lang.reflect.Member;
 import java.net.URI;
 import java.util.List;
 
